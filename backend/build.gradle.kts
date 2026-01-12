37f9fa8ad1f0f681bcdede122a32adf23ca8dfe6
Patrick Schönfeld <patrick.schoenfeld@googlemail.com>
2026-01-12 13:02:17 +0100

    use java 18

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
-    id("org.springframework.boot") version "4.0.1"
+    id("org.springframework.boot") version "3.3.6"
     id("io.spring.dependency-management") version "1.1.7"
-    kotlin("jvm") version "2.3.0"
-    kotlin("plugin.spring") version "2.3.0"
-    kotlin("plugin.jpa") version "2.3.0"
+    kotlin("jvm") version "1.9.25"
+    kotlin("plugin.spring") version "1.9.25"
+    kotlin("plugin.jpa") version "1.9.25"
     id("org.jlleitschuh.gradle.ktlint") version "14.0.1"
 }
 
@@ -12,7 +12,7 @@ version = "0.0.1-SNAPSHOT"
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(21))
+        languageVersion.set(JavaLanguageVersion.of(18))
     }
 }
 
