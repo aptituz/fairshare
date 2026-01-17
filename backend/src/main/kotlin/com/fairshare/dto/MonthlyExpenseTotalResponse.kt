4b5e4a5b6e5e29ccf4e10f70b77b966534bc5c49
Patrick Schönfeld <patrick.schoenfeld@googlemail.com>
2026-01-17 08:07:28 +0100

    Add yearly expense DTOs

---
+++ backend/src/main/kotlin/com/fairshare/dto/MonthlyExpenseTotalResponse.kt
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) 2025 Patrick Schoenfeld <patrick.schoenfeld@gmail.com>
+ * SPDX-License-Identifier: GPL-3.0-only
+ */
+
+package com.fairshare.dto
+
+import java.math.BigDecimal
+
+data class MonthlyExpenseTotalResponse(
+    val month: String,
+    val totalExpense: BigDecimal,
+)
